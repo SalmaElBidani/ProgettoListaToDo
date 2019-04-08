3a8388ba3e316e61e4df5982d93634c2e3b06843
ValeriaNardoni <valeria.nardoni@stud.unifi.it>
2019-04-08 18:18:40 +0200

    primo commit da pc di selma

---
+++ cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/Board.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/Board.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/Checklist.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/Checklist.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/Date.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/Date.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/MainWindow.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/MainWindow.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/Observer.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/Observer.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/SottoTask.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/SottoTask.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/Subject.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/Subject.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/Task.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/Task.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/TaskView.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/TaskView.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/Time.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/Time.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/User.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/User.cpp.o"
+  "/home/selma/Documenti/Programmazione/ProgettoListaToDo/main.cpp" "/home/selma/Documenti/Programmazione/ProgettoListaToDo/cmake-build-debug/CMakeFiles/ProgettoListaToDo.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
